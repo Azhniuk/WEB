bf7156ca78d3f389d1b2f5885e3a3c49c929882a
Sofia <azhniuk.s@gmail.com>
2022-06-14 21:53:56 +0300

    Change

--- build/src/weather.js
+++ build/src/weather.js
@@ -1,14 +1,10 @@
-
-
-
-
-function searchWeather(response){
+function searchWeather(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = `${Math.round(response.data.main.temp)}°C`;
-
+  document.querySelector("#temperature").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°C`;
 }
-
 
 function search(event) {
   event.preventDefault();
@@ -25,28 +21,23 @@ let apiKey = "9501a68da2d700b9b0fb729939635887";
 
 let cityName = document.querySelector("#city");
 
-
-
-
-
-
-
 let dateNow = document.querySelector("#date");
 let dayNow = document.querySelector("#day");
 let currentTime = new Date();
-
 
 let hours = currentTime.getHours();
 let minutes = currentTime.getMinutes();
 let dayNum = currentTime.getDay();
 
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-"Friday", "Saturday"]
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 
 
 dateNow.innerHTML = `${days[dayNum]}: ${hours}:${minutes}`;
-
-
-
-
-
